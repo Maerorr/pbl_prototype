3d4f50c241bc41c576c52ac4c1b7a1b4e6397d45
Mateusz Kowalski <236567@edu.p.lodz.pl>
2023-03-08 02:05:37 +0100

    (feat) Controlling Split
    "," = player fullscreen
    "." = split-screen
    "/" = hacker fullscreen

---
+++ pbl_prototype/Assets/Script/SplitScreenController.cs
@@ -0,0 +1,43 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SplitScreenController : MonoBehaviour
+{
+    [SerializeField] private Camera playerCamera;
+    [SerializeField] private Camera hackerCamera;
+
+    private Rect fullScreen;
+    private Rect noScreen;
+    private Rect splitLeft;
+    private Rect splitRight;
+    
+    // Update is called once per frame
+    private void Start()
+    {
+        fullScreen = new Rect(0.0f, 0.0f, 1.0f, 1.0f);
+        noScreen = new Rect(0.0f, 0.0f, 0.0f, 0.0f);
+        splitLeft = new Rect(0.0f, 0.0f, 0.5f, 1.0f);
+        splitRight = new Rect(0.5f, 0.0f, 0.5f, 1.0f);
+    }
+
+    void Update()
+    {
+        if (Input.GetKey(","))
+        {
+            playerCamera.rect = fullScreen;
+            hackerCamera.rect = noScreen;
+        }
+        else if (Input.GetKey((".")))
+        {
+            playerCamera.rect = splitLeft;
+            hackerCamera.rect = splitRight;
+        }
+        else if (Input.GetKey(("/")))
+        {
+            hackerCamera.rect = fullScreen;
+            playerCamera.rect = noScreen;
+        }
+    }
+}

0941a4903ec206496fb4949fcca839f7bc29d1fd
Mateusz Kowalski <236567@edu.p.lodz.pl>
2023-03-13 00:03:23 +0100

    fix: player max detection

--- pbl_prototype/Assets/Script/Player.cs
+++ pbl_prototype/Assets/Script/Player.cs
@@ -15,7 +15,7 @@ public class Player : MonoBehaviour
     
     [SerializeField] private Slider detectionSlider;
 
-    [SerializeField] private float maxDetectionLevel = 10.0f;
+    [SerializeField] private float maxDetectionLevel = 1.0f;
     // Start is called before the first frame update
     void Start()
     {

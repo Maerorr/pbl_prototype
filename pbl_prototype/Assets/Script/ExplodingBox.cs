1888627538c5a1f5b9d25bbe6bec3c7982756e89
Maerorr <7hubert7@gmail.com>
2023-03-12 10:24:20 +0100

    feat: simple explosion particles to juice things up

--- pbl_prototype/Assets/Script/ExplodingBox.cs
+++ pbl_prototype/Assets/Script/ExplodingBox.cs
@@ -7,12 +7,15 @@ public class ExplodingBox : HackableObject
     private bool HasExploded = false;
     
     [SerializeField] private float explosionRadius = 5f;
+    [SerializeField] private ParticleSystem explosionParticles;
 
     public override void OnHack()
     {
         HasExploded = true;
 
         var colliders = Physics.OverlapSphere(transform.position, explosionRadius);
+        
+        explosionParticles.Play();
         
         for (int i = 0; i < colliders.Length; i++)
         {
